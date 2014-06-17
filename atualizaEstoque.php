42f2d68911f98e7078ebbca52f6a772fd67434c1
Pascoal <pascoal.vernieri@plathanus.com.br>
2014-06-17 11:22:12 -0300

    incluindo label

--- atualizaEstoque.php
+++ atualizaEstoque.php
@@ -8,7 +8,7 @@
 <body>
 
         <form name="form" action="updateEstoque.php" method="post" enctype="multipart/form-data">
-            <label>Arquivo Integracao Estoque:</label>
+            <label>Arquivo para integração Estoque:</label>
 				<input type="file" id="arquivo" name="arquivo" />
 					</p>
 				
